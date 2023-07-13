abff90bafd69f70c329fd984dcdc0b34d7618d2a
koushikreddypodduturi <koushikreddypodduturi@gmail.com>
2023-07-13 12:57:21 -0400

    bug fixed 121

--- src/main/java/in/ashokit/security/PasswordService.java
+++ src/main/java/in/ashokit/security/PasswordService.java
@@ -11,12 +11,15 @@ int i=10;
 		return encoder.encodeToString(txt.getBytes());
 	}
 	public static String decode(String encodeTxt) {
+		//changes for bug 121
+				int k=50;
 		Decoder decoder = Base64.getDecoder();
 		byte[] decode = decoder.decode(encodeTxt);
 		return new String(decode);
 	}
 	public static void main(String[] args)
 	{
+		
 		String encode=PasswordService.encode("ashokit");
 		System.out.println(encode);
 		String decode=PasswordService.decode(encode);
